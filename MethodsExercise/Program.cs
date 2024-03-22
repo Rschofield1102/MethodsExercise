3a3bb7a71bf596603fad912abb5af6ab59d3a5da
Robert Schofield <rschofield1992@gmail.com>
2024-03-22 16:15:10 -0400

    first

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           // Name: Robert
+           // Favorite Color: Blue
+           // Favorite Animal: Dog
+           // Favorite Band: Any
+
+
+            Console.WriteLine($"Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"Okay {color}. Now what is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"Good,and what is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
